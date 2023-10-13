9d5c87408f19d381dd4fbdac29a0b53b94ff6d4f
victorjurj <victor.jurj@yahoo.com>
2023-10-13 10:20:24 +0300

    add HotelsController

---
+++ src/main/java/com/fortech/academy/library/controller/HotelsController.java
@@ -0,0 +1,15 @@
+package com.fortech.academy.library.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hotels")
+public class HotelsController {
+
+    @GetMapping("test")
+        public String test(){
+        return "It works...";
+    }
+}
