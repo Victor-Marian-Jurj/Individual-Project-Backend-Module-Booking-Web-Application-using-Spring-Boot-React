b488dd6bfde3c8df7fc0684c9221c0e38b8d4cdf
victorjurj <victor.jurj@yahoo.com>
2023-10-16 10:30:40 +0300

    add class CreateRoomRequest and RoomsRepository interface

---
+++ src/main/java/com/fortech/academy/library/controller/CreateRoomRequest.java
@@ -0,0 +1,47 @@
+package com.fortech.academy.library.controller;
+
+import jakarta.persistence.Column;
+
+public class CreateRoomRequest {
+
+    private int roomNumber;
+
+    private String roomType;
+
+    private int roomFloor;
+
+    private int roomPrice;
+
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getRoomFloor() {
+        return roomFloor;
+    }
+
+    public void setRoomFloor(int roomFloor) {
+        this.roomFloor = roomFloor;
+    }
+
+    public int getRoomPrice() {
+        return roomPrice;
+    }
+
+    public void setRoomPrice(int roomPrice) {
+        this.roomPrice = roomPrice;
+    }
+}
