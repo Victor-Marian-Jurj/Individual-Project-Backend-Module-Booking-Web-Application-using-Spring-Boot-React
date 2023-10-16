eda2a02f45fe9b8ceba5975b5f89e973c8f0f734
victorjurj <victor.jurj@yahoo.com>
2023-10-16 10:28:37 +0300

    add RoomsController and readAllRooms method

--- src/main/java/com/fortech/academy/library/controller/RoomsController.java
+++ src/main/java/com/fortech/academy/library/controller/RoomsController.java
@@ -1,19 +1,37 @@
 package com.fortech.academy.library.controller;
 
 
+import com.fortech.academy.library.entities.Room;
+import com.fortech.academy.library.service.RoomsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("rooms")
 public class RoomsController {
 
-    @GetMapping("test")
-        public String test() {
-            return "It works...";
-        }
+    private final RoomsService roomsService;
+
+    @Autowired
+    public RoomsController(RoomsService roomsService) {
+        this.roomsService = roomsService;
     }
+
+
+    @GetMapping("test")
+    public String test() {
+        return "It works...";
+    }
+
+    @GetMapping
+    public List<Room> readAllRooms() {
+        return roomsService.getAllRooms();
+    }
+}
 
 
 
