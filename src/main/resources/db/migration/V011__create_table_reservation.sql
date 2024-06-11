8138ebbdaa1342a7b4457f61af5cb9d5f11a3f35
Victor Marian Jurj <victor.jurj@yahoo.com>
2024-06-11 18:14:56 +0300

    modify tables insert and create files

---
+++ src/main/resources/db/migration/V011__create_table_reservation.sql
@@ -0,0 +1,17 @@
+CREATE TABLE reservation (
+reservation_id serial PRIMARY KEY,
+hotel_id INTEGER NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(20) NOT NULL,
+phone_number VARCHAR(20) NOT NULL UNIQUE,
+email_address VARCHAR(50) NOT NULL UNIQUE,
+check_in_date DATE NOT NULL,
+check_out_date DATE NOT NULL,
+room_type VARCHAR(20) NOT NULL,
+room_price INTEGER NOT NULL,
+payment_method VARCHAR(20) NOT NULL,
+total_payment INTEGER NOT NULL,
+CONSTRAINT fk_reservation_hotels
+FOREIGN KEY (hotel_id)
+REFERENCES hotels (hotel_id)
+);
