a19c337b41c0f7e46dcece5bf3358d05af273f2b
Victor Marian Jurj <victor.jurj@yahoo.com>
2024-07-02 13:50:54 +0300

    adjust table data and rename table

--- src/main/resources/db/migration/V009__create_table_payments.sql
+++ src/main/resources/db/migration/V009__create_table_payments.sql
@@ -1,11 +1,11 @@
-CREATE TABLE payments (
-payment_id serial PRIMARY KEY,
-name_on_card VARCHAR(50) NOT NULL,
-card_number BIGINT NOT NULL,
-expiration_date Date NOT NULL,
-cvc_number INTEGER NOT NULL
-);
---user_id INTEGER NOT NULL,
---CONSTRAINT fk_payment_users
---FOREIGN KEY (user_id)
---REFERENCES users (user_id)
+--CREATE TABLE payments (
+--payment_id serial PRIMARY KEY,
+--name_on_card VARCHAR(50) NOT NULL,
+--card_number BIGINT NOT NULL,
+--expiration_date Date NOT NULL,
+--cvc_number INTEGER NOT NULL
+--);
+----user_id INTEGER NOT NULL,
+----CONSTRAINT fk_payment_users
+----FOREIGN KEY (user_id)
+----REFERENCES users (user_id)
